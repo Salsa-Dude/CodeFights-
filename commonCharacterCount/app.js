ab9ef6b34280946e120912a0b75c476653dc6567
Joseph Arias <arias.joseph.a@gmail.com>
2018-06-11 13:55:32 -0400

    commonCharacterCount

--- commonCharacterCount/app.js
+++ commonCharacterCount/app.js
@@ -2,16 +2,41 @@
 // Given two strings, find the number of common characters between them.
 
 function commonCharacterCount(s1, s2) {
+// Split argument into arrays
+ s1 = s1.split('');
+ s2 = s2.split('');
+ s1Object = {};
+ s2Object = {};
+ let total = 0;
 
-  const placeHolder = [];
-
-  for(let i = 0; i < s1.length; i++) {
-    for(let j = 0; j > s2.length; j++) {
-      
+ // Loop thru array and set object keys and properties
+function objectProperty(arr, obj) {
+  for(let i = 0; i < arr.length; i++) {
+    if(obj.hasOwnProperty(arr[i]) === false) {
+      obj[arr[i]] = 1;
+    } else {
+      obj[arr[i]]++;
     }
   }
-  
-  // return placeHolder;
+ }
+ 
+ objectProperty(s1,s1Object);
+ objectProperty(s2,s2Object);
+
+ // Loop thru object and compare each prop
+ // if both objects contain the same prop
+ // Add the least value of the two props to total
+ for(let prop in s1Object){
+   if(s2Object.hasOwnProperty(prop) === true) {
+     if(s2Object[prop] < s1Object[prop]) {
+       total += s2Object[prop];
+     } else {
+       total += s1Object[prop];
+     }
+   }
+ }
+
+ return total;
 
 }
 
