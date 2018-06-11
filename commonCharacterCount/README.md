966210ac59adafa934eae1a3336c79c34f837fd1
Joseph Arias <arias.joseph.a@gmail.com>
2018-06-10 23:56:51 -0400

    commonCharacter

---
+++ commonCharacterCount/README.md
@@ -0,0 +1,8 @@
+
+<h2>commonCharacterCount</h2>
+
+<p>Given two strings, find the number of common characters between them.</p>
+<strong>Example</strong>
+For s1 = "aabcc" and s2 = "adcaa", the output should be <br>
+commonCharacterCount(s1, s2) = 3. <br>
+Strings have 3 common characters - 2 "a"s and 1 "c".
---
+++ commonCharacterCount/app.js
@@ -0,0 +1,18 @@
+
+// Given two strings, find the number of common characters between them.
+
+function commonCharacterCount(s1, s2) {
+
+  const placeHolder = [];
+
+  for(let i = 0; i < s1.length; i++) {
+    for(let j = 0; j > s2.length; j++) {
+      
+    }
+  }
+  
+  // return placeHolder;
+
+}
+
+console.log(commonCharacterCount('aabcc', 'adcaa'));
