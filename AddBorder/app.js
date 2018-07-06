980d16dfdf2ec7d8ba5e93411abf71f19a9ba226
Joseph Arias <arias.joseph.a@gmail.com>
2018-07-06 00:02:00 -0400

    fix error

--- AddBorder/app.js
+++ AddBorder/app.js
@@ -2,7 +2,7 @@
 // Given a rectangular matrix of characters, add a border of asterisks(*) to it.
 
 function addBorder(picture) {
-  const lengthOfWall = picture.length[0].length + 2;
+  const lengthOfWall = picture[0].length + 2;
   let wall = "";
 
   for(let i = 0; i < lengthOfWall; i++) {
@@ -15,6 +15,8 @@ function addBorder(picture) {
   for(let j = 0; j < picture.length -1; j++) {
     picture[j] = '*'.concat(picture[j], '*');
   }
+
+  return picture;
 }
 
 console.log(addBorder(["abc", "ded"]));
